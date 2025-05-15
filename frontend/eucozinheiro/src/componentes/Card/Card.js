49cb8e9b6b8ec21ffbe06965aa3c5263d098e2b3
Ricardo Silva <7474175+romstk@users.noreply.github.com>
2025-05-14 22:58:56 -0300

    Atualização do componente Card.js, atualizando a constante base url para o caminho do site para a pasta media para visualizar as imagens das receitas

--- frontend/eucozinheiro/src/componentes/Card/Card.js
+++ frontend/eucozinheiro/src/componentes/Card/Card.js
@@ -3,16 +3,22 @@ import './Card.css';
 
 function Card({imagem, nome, descricao, autor }) {
     // baseURL é a URL base para as imagens que está sendo salva no backend quando salvamos as receitas o Django salva as imagens no diretório media pois foi definido no settings.py
-    const baseURL = 'http://localhost:8000/media/';
+    const baseURL = 'http://www.eucozinheiro.com.br/media/';
     
     return (
         <div class="receitas__card">
             
             {imagem === null ? (
+                // Se a imagem for null, vamos carregar uma imagem padrão chamada not-found.png.
+                // A imagem padrão deve estar no diretório public/imagens/not-found.png, dentro do projeto React.
                 <img class="receitas__imagem" src={process.env.PUBLIC_URL + '/imagens/' + 'not-found.png'} alt={nome} />
                 
 
             ) :
+                // Se a imagem não for null, vamos carregar a imagem do diretório media do backend.
+                // A imagem deve estar no diretório media do backend, que foi definido no settings.py do Django.
+                // A imagem é carregada com a URL base + o nome da imagem.
+                // O nome da imagem é salvo no banco de dados quando a receita é cadastrada.
                 <img class="receitas__imagem" src={ baseURL + imagem } alt={nome} />                         
             } 
 
